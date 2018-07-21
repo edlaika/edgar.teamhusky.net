cbc694fa6e97f1e5615a5326cf772795a112a53f
Edgar <edgar.axelsson@hotmail.com>
2018-07-21 19:45:13 +0200

    New Website
    Design and domain.

--- js/custom-script.js
+++ js/custom-script.js
@@ -3,12 +3,36 @@ $(document).ready(function() {
 	$('.parallax').parallax();
 	$('.scrollspy').scrollSpy();
 	$('ul.tabs').tabs();
+	$('.modal').modal();
+	$('.fixed-action-btn').floatingActionButton();
+	//scrollUp
+	$(function toTop() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('#top').fadeIn();
+			} else {
+				$('#top').fadeOut();
+			}
+		});
+		$('#top').click(function() {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
 });
-
-//typed
+//typed main
 $(function() {
 	$(".element").typed({
-		strings: ["Java.", "HTML.", "SQL.", "GIT.", "UML.", "PHP.", "CSS.", "JS.", "Web Developer.", "Freelancer.", "husky", "edlaika", "Edgar"],
+		strings: ["I like Anime.", "I enjoy Programming.", "I love Design.", "Graphic Designer", "Web developer"],
+		typeSpeed: 40
+	});
+});
+//typed socialising
+$(function() {
+	$(".social").typed({
+		strings: ["I can build a website for you!", "I can fix your computer!", "I can provide advice for you.", "I can help you with research.", "I can manage your server or infrastructure or offer support.", "I can make your idea a reality.", "Don't be shy and contact me! :)"],
 		typeSpeed: 40
 	});
 });
@@ -30,13 +54,6 @@ $("#appanimate").click(function() {
 $("#otheranimate").click(function() {
 	$("#otheranimation").addClass("wow fadeInUp animated");
 	$("#otheranimation").attr("style", "visibility: visible; animation-name: fadeInUp;");
-});
-// scrollUp
-$(function() {
-	$.scrollUp({
-		animation: 'slide',
-		scrollText: '<i class="fa fa-angle-up" aria-hidden="true"></i>', // Text for element
-	});
 });
 var loc = window.location.href + '';
 if (loc.indexOf('http://') === 0) {
